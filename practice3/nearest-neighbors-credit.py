48b8e0a1db83c9ee9544c4b3feb49382a92bf643
David Martinez <34782922+dmtzt@users.noreply.github.com>
2021-09-03 16:17:58 -0500

    k-nearest neighbors with scatter plot

--- practice3/nearest-neighbors-credit.py
+++ practice3/nearest-neighbors-credit.py
@@ -1,21 +1,26 @@
 import numpy as np
 import pandas as pd
-from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.model_selection import train_test_split
 
 
-# Function to normalize data
+# Normalize data in range [0, 1]
 def normalize(atrib):
     vmax = max(atrib)
     vmin = min(atrib)
     return (atrib - vmin)/(vmax - vmin)
 
 
-def nearest_neighbors(n_neighbors):
-    return 0
+# Turn 'Yes'/'No' strings into boolean values
+def yes_no_boolean(array):
+    return (array == 'Yes')
 
 
 filepath = 'credit.txt'
-n_neighbors = [1, 2, 3, 5, 10, 15, 20, 50, 75, 100]
+n_neighbors_values = [1, 2, 3, 5, 10, 15, 20, 50, 75, 100]
 
 # Load credit dataset
 data = pd.read_csv(filepath, sep='\t', header=0)
@@ -23,12 +28,56 @@ data = pd.read_csv(filepath, sep='\t', header=0)
 student = data['student']
 balance = data['balance']
 income = data['income']
-# Turn Yes/No values into bool values
-print(student)
-# # default is the output
-# default = data['default']
-# # Normalize inputs
-# balance = normalize(balance)
-# income = normalize(income)
-# # Join all inputs
-# X = np.stack((student, balance, income), axis=1)
+# default is the output
+default = data['default']
+
+# Turn 'Yes'/'No' strings into boolean values
+student = yes_no_boolean(student)
+default = yes_no_boolean(default)
+
+# Normalize inputs
+balance = normalize(balance)
+income = normalize(income)
+
+# Join all inputs
+X = np.stack((student, balance, income), axis=1)
+
+# Make train and test sets (80%-20%)
+x_train, x_test, y_train, y_test = train_test_split(
+    X, default, test_size=0.2, train_size=0.8, random_state=1, shuffle=True)
+
+plt.title('k-neighbors vs precision rate')
+plt.xlabel('k-neighbors')
+plt.ylabel('precision score')
+
+# K and ps values lists
+k_list = []
+ps_list = []
+
+# Create 10 k-nearest neighbors classifiers with predetermined values
+for k in n_neighbors_values:
+    # Create classifier with respective k value
+    nbrs = KNeighborsClassifier(n_neighbors=k, algorithm='brute')
+    # Fit the classifier from the train set
+    nbrs.fit(x_train, y_train)
+    # Predict class labels for test test
+    pred = nbrs.predict(x_test)
+    # Compute precision score according to true and false positive
+    ps = precision_score(pred, y_test)
+    # Compute precision matrix to evaluate accuracy
+    cmat = confusion_matrix(pred, y_test)
+    # Print results
+    print('K:{k}\tps:{ps}\tcmat{cmat}'.format(k=k, ps=ps, cmat=cmat))
+
+    # Append k value and precision score to their lists
+    k_list.append(k)
+    ps_list.append(ps)
+
+# Scatter plot of k vs ps values
+plt.scatter(k_list, ps_list)
+plt.plot(k_list, ps_list)
+
+# Save plot
+plt.savefig('plot_k-neighbors_precision_score.png',
+            dpi=300, bbox_inches='tight')
+plt.clf()
