200d159213c9da1c967f04e549a947912a2148d9
David Martinez <34782922+dmtzt@users.noreply.github.com>
2021-10-02 22:33:33 -0500

    practice 6 - completed svm and added k-NN model

--- practice6/svm.py
+++ practice6/svm.py
@@ -1,6 +1,8 @@
 from sklearn import svm
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
 
 digits = load_digits()
 X = digits.data
@@ -8,13 +10,83 @@ y = digits.target
 x_train, x_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, train_size=0.8, random_state=1, shuffle=True)
 
+# SVM models
+# Linear kernel: linearly separable data (faster)
 linear_svc = svm.SVC(kernel='linear')
+# Polynomial kernel: polynomial decision boundary using combinations of data points (less efficient)
 poly_svc = svm.SVC(kernel='poly')
+# Radial Basis Function (RBF) kernel: non linearly separable data (most popular and efficient)
 rbf_svc = svm.SVC(kernel='rbf')
+# Sigmoid kernel: uses sigmoid function, widely used in neural networks
 sigmoid_svc = svm.SVC(kernel='sigmoid')
 
-# Train models
-linear_svc.fit(X, y)
-poly_svc.fit(X, y)
-rbf_svc.fit(X, y)
-sigmoid_svc.fit(X, y)
+# Train SVM models
+linear_svc.fit(x_train, y_train)
+poly_svc.fit(x_train, y_train)
+rbf_svc.fit(x_train, y_train)
+sigmoid_svc.fit(x_train, y_train)
+
+# Get SVM models predictions
+linear_pred = linear_svc.predict(x_test)
+poly_pred = poly_svc.predict(x_test)
+rbf_pred = rbf_svc.predict(x_test)
+sigmoid_pred = sigmoid_svc.predict(x_test)
+
+# Get SVM models precision scores
+linear_ps = linear_svc.score(x_test, y_test)
+poly_ps = poly_svc.score(x_test, y_test)
+rbf_ps = rbf_svc.score(x_test, y_test)
+sigmoid_ps = sigmoid_svc.score(x_test, y_test)
+
+# Get SVM models confusion matrices
+linear_cmat = confusion_matrix(linear_pred, y_test)
+poly_cmat = confusion_matrix(poly_pred, y_test)
+rbf_cmat = confusion_matrix(rbf_pred, y_test)
+sigmoid_cmat = confusion_matrix(sigmoid_pred, y_test)
+
+# Logistic regression model
+# Usar valor 'ovr'
+
+# k-NN models
+# Neighbors, K, precision score and prediction with best performance
+n_neighbors_values = [1, 2, 3, 5, 10, 15, 20, 50, 75, 100]
+k_best = 0
+ps_best = 0
+pred_best = []
+cmat_best = []
+knn_bestmodel = None
+# Create 10 k-nearest neighbors classifiers with predetermined values
+for k in n_neighbors_values:
+    # Create classifier with respective k value
+    nbrs = KNeighborsClassifier(n_neighbors=k, algorithm='brute')
+    # Fit the classifier from the train set
+    nbrs.fit(x_train, y_train)
+    # Predict class labels for test test
+    pred = nbrs.predict(x_test)
+    # Compute precision score according to true and false positive
+    ps = nbrs.score(x_test, y_test)
+    # Compute confusion matrix to evaluate accuracy
+    cmat = confusion_matrix(pred, y_test)
+
+    # Find k with best precision score
+    if (ps > ps_best):
+        k_best = k
+        ps_best = ps
+        pred_best = pred
+        cmat_best = cmat
+        knn_bestmodel = nbrs
+
+# Naive-Bayes model
+
+
+# Print scores and confusion matrices
+print(
+    'Linear SVM - ps: {ps} - confusion matrix:\n{cmat}'.format(ps=linear_ps, cmat=linear_cmat))
+print(
+    'Polynomial SVM - ps: {ps} - confusion matrix:\n{cmat}'.format(ps=poly_ps, cmat=poly_cmat))
+print(
+    'RBF SVM - ps: {ps} - confusion matrix:\n{cmat}'.format(ps=rbf_ps, cmat=rbf_cmat))
+print(
+    'Sigmoid SVM - ps: {ps} - confusion matrix:\n{cmat}'.format(ps=sigmoid_ps, cmat=sigmoid_cmat))
+print(
+    'k-NN - best k: {k} - ps: {ps} - confusion matrix:\n{cmat}'.format(k=k_best, ps=ps_best, cmat=cmat_best))
